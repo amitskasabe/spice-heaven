77d955f63af6cd3a628955dfaebcbf1445a08646
amitkasabe <amit.kasabe25@gmail.com>
2024-02-12 17:00:23 +0530

    first commit

---
+++ components/navbar.php
@@ -0,0 +1,17 @@
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Protest+Revolution&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="assets/css/common.css">
+<link rel="stylesheet" href="assets/css/nav.css">
+
+<section id="navbar">
+    <nav class="flex justify-between items-center space-p-x-10" id="main-nav">
+        <div class="nav-brand text-white"> <h4>Spaice Heaven</h4> </div>
+        <ul class="flex justify-center items-center " id="nav-items">
+            <li><a href="" class="decoration-none camleCase weight-bold text-black space-10 text-white">Home</a></li>
+            <li><a href="" class="decoration-none camleCase weight-bold text-black space-10 text-white"> Find Us </a></li>
+            <li><a href="" class="decoration-none camleCase weight-bold text-black space-10 text-white"> Perfect Companion </a></li>
+        </ul>
+    </nav>
+</section>
---
+++ hero.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="assets/css/hero.css">
+<link rel="stylesheet" href="assets/css/common.css">
+<section id="hero">
+    <div class="box"></div>
+    <?php include 'components/navbar.php' ?>
+    <div class="hero-content">
+        <img src="assets/images/biryani.png" class="vert-move" alt="no image">
+        <img src="assets/images/delivery-boy.png" class="" id="delivery-partner" alt="no image">
+        <div id="ingradients">
+            <ul>
+                <li> Chicken / Mutton </li>
+                <li> Rice </li>
+                <li> Tomatos </li>
+                <li> Oragano </li>
+            </ul>
+        </div>
+        <h1 class="hero-text text-white "> Welcome To Spice Heaven </h1>
+    </div>
+    <div>
+     
+    </div>
+</section>
